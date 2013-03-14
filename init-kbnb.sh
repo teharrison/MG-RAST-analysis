8c9329e2e4f1fb2326e5b5c2b7645b65a9f471a8
Travis Harrison <guumby@gmail.com>
2013-03-14 11:41:19 -0500

    updated start scripts

--- init-kbnb.sh
+++ init-kbnb.sh
@@ -1,59 +1,62 @@
 #!/bin/bash                                                                                                                                             
 
+HELP=0
 SHOCK_USER='public'
-SHOCK_URL='http://140.221.84.125:8000'
+SHOCK_SERVER='http://140.221.84.125:8000'
 BUILD_MODE='none'
 DEV_DIR=/kb/dev_container
 
-if [ -n "$1" ]; then
-    if [[ "$1" == *"-h"* ]]; then
-        echo "Usage: init_kbnb SHOCK_USER($SHOCK_USER) SHOCK_URL($SHOCK_URL) BUILD_MODE(none|ipython|all)"
-        exit
-    else
-        SHOCK_USER=$1
-    fi
-fi
-if [ -n "$2" ]; then
-    SHOCK_URL=$2
-fi
-if [ -n "$3" ]; then
-    BUILD_MODS=$3
+# get args
+while getopts hu:s:b: option; do
+    case "${option}"
+	    in
+	    h) HELP=1;;
+	    u) SHOCK_USER=${OPTARG};;
+	    s) SHOCK_SERVER=${OPTARG};;
+	    b) BUILD_MODE=${OPTARG};;
+    esac
+done
+
+# help
+if [ $HELP -eq 1 ]; then
+    echo "Usage: init-kbnb.sh [-h] -u SHOCK_USER -s SHOCK_SERVER -b BUILD_MODE(none|ipython|all)"
+    exit
 fi
 
-# get the kbase env                                                                                                                                     
+# get the kbase env
 source $DEV_DIR/user-env.sh
 
-# re-build kbase modules                                                                                                                                
+# re-build kbase modules
 if [ $BUILD_MODE == 'all' ]; then
     cd $DEV_DIR/modules
-    # update repos                                                                                                                                      
+    # update repos
     for M in *; do
         pushd $M
         git pull origin master
         popd
     done
-    # make deploy                                                                                                                                       
+    # make deploy
     cd $DEV_DIR
     make
     make deploy
 fi
 
-# re-build just analysis_book                                                                                                                           
+# re-build just analysis_book
 if [ $BUILD_MODE == 'ipython' ]; then
     cd $DEV_DIR/modules/analysis_book
     git pull origin master
     make deploy-server
 fi
 
-# we don't want nginx running                                                                                                                           
+# we don't want nginx running
 /etc/init.d/nginx stop
 
-# place notebook dir in /mnt                                                                                                                            
+# place notebook dir in /mnt
 mv /kb/deployment/services/analysis_book/notebook /mnt/notebook
 ln -s /mnt/notebook /kb/deployment/services/analysis_book/notebook
 chown -R ipython:ipython /mnt/notebook
 
-# start notebook                                                                                                                                        
+# start notebook
 /kb/deployment/services/analysis_book/stop_service
-/kb/deployment/services/analysis_book/start_service $SHOCK_USER $SHOCK_URL
+/kb/deployment/services/analysis_book/start_service $SHOCK_USER $SHOCK_SERVER
 
