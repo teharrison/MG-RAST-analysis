d101244afe991dc0c877d132dae7ea98a38d33ff
Travis Harrison <guumby@gmail.com>
2013-03-19 12:20:42 -0500

    more custom notebook js

--- custom/custom.js
+++ custom/custom.js
@@ -6,6 +6,10 @@ mainly to be overridden in profile/static/js/custom.js
 This will always be an empty file in IPython
 */
 
+// hide header and toolbar-buttons
+document.getElementById("header").style.display="none";
+document.getElementById("maintoolbar").style.display="none";
+
 // receive messages sent from other frames
 window.addEventListener("message", receiveMessage, false);
 function receiveMessage(event) {
