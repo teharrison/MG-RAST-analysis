84fbe648e33c5d100b0704698d12ee802f8e4bc7
Travis Harrison <guumby@gmail.com>
2013-11-15 13:32:19 -0600

    simplified cleanup script

--- clean-ipython.sh
+++ clean-ipython.sh
@@ -2,4 +2,8 @@
 
 # clean out old version of ipython if exists
 pip uninstall -y ipython
-cat conf/ipython_files.txt | xargs rm -rfv
+rm -rfv /usr/local/lib/python2.7/dist-packages/IPython
+rm -rfv /usr/local/share/doc/ipython
+rm -rfv /usr/local/lib/python2.7/dist-packages/ipython*
+rm -fv /usr/local/bin/ipython
+rm -fv /usr/local/share/man/man1/ipython.1
